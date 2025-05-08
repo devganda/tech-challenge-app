b6e3b56f64b6be0f39807f3f06634e7efe7ff20c
Danilo Ganda <daniloganda95@gmail.com>
2025-05-08 11:41:22 -0300

    refactor: aprimorar a rota de criação de usuário com validação de esquema e documentação

--- src/routes/UserRoute.ts
+++ src/routes/UserRoute.ts
@@ -3,23 +3,59 @@ import { CreateUser } from '@application/use-cases/CreateUser';
 import { PrismaUserRepository } from '@infrastructure/repositories/PrismaUserRepository';
 import { UserMapper } from '../infrastructure/mappers/UserMapper';
 
-export async function userRoutes(app: FastifyInstance) {
+export default async function UserRoute(app: FastifyInstance) {
    const userRepository = new PrismaUserRepository();
 
-   app.post('/', async (request, reply) => {
-      const createUser = new CreateUser(userRepository);
+   app.post('/',
+      {
+         schema: {
+            summary: 'Cria um novo usuário',
+            description: 'Este endpoint cria um novo usuário no sistema',
+            tags: ['Users'],
+            body: {
+               type: 'object',
+               properties: {
+                  name: { type: 'string', description: 'Nome do usuário' },
+                  email: { type: 'string', format: 'email', description: 'Email do usuário' }
+               },
+               required: ['name', 'email']
+            },
+            response: {
+               201: {
+                  description: 'Usuário criado com sucesso',
+                  type: 'object',
+                  properties: {
+                     id: { type: 'string', description: 'ID do usuário' },
+                     name: { type: 'string', description: 'Nome do usuário' },
+                     email: { type: 'string', format: 'email', description: 'Email do usuário' }
+                  }
+               },
+               400: {
+                  description: 'Erro ao criar o usuário',
+                  type: 'object',
+                  properties: {
+                     error: { type: 'string', description: 'Mensagem de erro' }
+                  }
+               }
+            }
+         }
+      },
 
-      const { name, email } = request.body as {
-         name: string;
-         email: string;
-      };
+      async (request, reply) => {
+         const createUser = new CreateUser(userRepository);
 
-      try {
-         const user = await createUser.execute({ name, email });
+         const { name, email } = request.body as {
+            name: string;
+            email: string;
+         };
 
-         return reply.status(201).send(UserMapper.toHTTP(user));
-      } catch (error: any) {
-         return reply.status(400).send({ error: error.message });
+         try {
+            const user = await createUser.execute({ name, email });
+
+            return reply.status(201).send(UserMapper.toHTTP(user));
+         } catch (error: any) {
+            return reply.status(400).send({ error: error.message });
+         }
       }
-   });
+   );
 }
