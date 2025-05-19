0fcd71da2d54fa8385a52984e2a591f5046c84cf
Danilo Ganda <daniloganda95@gmail.com>
2025-05-19 19:15:00 -0300

    feat: adiciona rotas para gerenciamento de clientes, produtos, pedidos, administração e pagamentos

--- src/infrastructure/http/server.ts
+++ src/infrastructure/http/server.ts
@@ -1,10 +1,15 @@
 import Fastify from 'fastify';
-import UserRoute from '@routes/UserRoute';
 import dotenv from 'dotenv';
 import fastifySwagger from '@fastify/swagger';
 import fastifySwaggerUI from '@fastify/swagger-ui';
 import Redis from '@infrastructure/redis/redis'
 import { prisma } from '@infrastructure/database/prisma';
+import UserRoute from '@routes/UserRoute';
+import ProductRoute from '@routes/ProductRoute';
+import PaymentRoute from '@routes/PaymentRoute';
+import AdminRoute from '@routes/AdminRoute';
+import OrderRoute from '@routes/OrderRoute';
+import CustomerRoute from '@routes/CustomerRoute';
 
 dotenv.config();
 
@@ -38,11 +43,17 @@ export async function startServer() {
 
    app.get('/', async () => {
       await Redis.set('welcome', 'Hello teach challenge!')
-      const message = await Redis.get('welcome')
+      const message = await Redis.get('welcome');
       return { message }
    });
 
    app.register(UserRoute, { prefix: '/users' });
+   app.register(CustomerRoute, { prefix: '/customer' });
+   app.register(ProductRoute, { prefix: '/products' });
+   app.register(OrderRoute, { prefix: '/orders' });
+   app.register(AdminRoute, { prefix: '/admin' });
+   app.register(PaymentRoute, { prefix: '/payment' });
+
 
    /*disconecta o Prisma quando o servidor for fechado*/
    app.addHook('onClose', async () => {
