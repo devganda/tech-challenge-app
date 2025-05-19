6e9715df5107c770a18ca7d1a8692fca3f16d0d0
Danilo Ganda <daniloganda95@gmail.com>
2025-05-19 19:13:16 -0300

    feat: adiciona validação e formatação para nomes na classe Name

--- src/domain/value-objects/Name.ts
+++ src/domain/value-objects/Name.ts
@@ -1,21 +1,37 @@
 export class Name {
    constructor(private readonly value: string) {
+      this.value = Name.format(value);
       this.validate();
    }
 
    private validate() {
       const nameRegex = /^[a-zA-Z\s]+$/;
-      if (!nameRegex.test(this.value)) {
-         throw new Error('Invalid name format');
+
+      if (!this.value || this.value.trim().length === 0) {
+         throw new Error('Nome não pode ser vazio');
       }
 
-      // Deixar sempre a primeira letra Maiuscula e o resto minusculo
-      this.value.split(' ')
-         .map(word => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase())
+      if (!nameRegex.test(this.value)) {
+         throw new Error('Formato de nome inválido');
+      }
+
+   }
+
+   private static format(value: string): string {
+      return value
+         .trim()
+         .split(/\s+/)
+         .map(
+            (word) => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()
+         )
          .join(' ');
    }
 
    public getValue(): string {
       return this.value;
+   }
+
+   public equals(other: Name): boolean {
+      return this.value === other.getValue();
    }
 }
